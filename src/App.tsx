01da2ce46e70c607c95eff243e0b6af4e44714e3
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-12-22 17:26:23 +0900

    feat : #4 로컬스토리지에서 데이터를 가져올 때 데이터가 손상된 경우를 처리할 수 있도록 에러 핸들링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from 'react';
 import Header from './components/Header';
 import DrawingToolBar from './components/DrawingToolBar';
 import { DrawingCanvas } from './components/DrawingCanvas';
+import { KonvaEventObject } from 'konva/lib/Node';
 
 export type TShapes = {
   drawMode: string;
@@ -16,34 +17,55 @@ export type TShapes = {
 };
 
 const App = () => {
-  // 현재 그려진 도형 리스트
-  const [shapes, setShapes] = useState<TShapes[]>(() =>
-    JSON.parse(localStorage.getItem('shapes') || '[]')
-  );
-  // 현재 그려지고 있는 도형
+  const [shapes, setShapes] = useState<TShapes[]>(() => {
+    try {
+      const storedShapes = localStorage.getItem('shapes');
+      return storedShapes ? JSON.parse(storedShapes) : [];
+    } catch (error) {
+      console.error('Failed to parse shapes from localStorage:', error);
+      return [];
+    }
+  });
+  // 사용자가 현재 그리고 있는 도형.
   const [currentShape, setCurrentShape] = useState<TShapes>({
     drawMode: 'freeDraw',
     points: [0, 0],
     fillColor: '#FFFFFF',
     strokeWidth: 5,
   });
+  // 사용자가 현재 그리고 있는지 여부
   const [isDrawing, setIsDrawing] = useState<boolean>(false);
+  // Redo 기능을 위한 실행 취소된 도형의 히스토리
   const historyStep = useRef<TShapes[]>([]);
 
   const handleChangeShapeMode = (mode: string) => {
-    const isPolygonMode = mode === 'polygon';
-
-    setIsDrawing(isPolygonMode);
-    setCurrentShape({
-      drawMode: mode,
-      points: isPolygonMode ? [-50, -50] : [],
-      fillColor: currentShape.fillColor,
-      strokeWidth: currentShape.strokeWidth,
-      radiusX: 0,
-      radiusY: 0,
-      width: 0,
-      height: 0,
-    });
+    switch (mode) {
+      case 'polygon':
+        setIsDrawing(true);
+        setCurrentShape({
+          drawMode: mode,
+          points: [-50, -50],
+          fillColor: currentShape.fillColor,
+          strokeWidth: currentShape.strokeWidth,
+          radiusX: 0,
+          radiusY: 0,
+          width: 0,
+          height: 0,
+        });
+        break;
+      default:
+        setIsDrawing(false);
+        setCurrentShape({
+          drawMode: mode,
+          points: [],
+          fillColor: currentShape.fillColor,
+          strokeWidth: currentShape.strokeWidth,
+          radiusX: 0,
+          radiusY: 0,
+          width: 0,
+          height: 0,
+        });
+    }
   };
 
   const handleChangeShapeColor = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -57,141 +79,141 @@ const App = () => {
   };
 
   const handleChangeStrokeWidth = (strokeWidth: number) => {
-    setCurrentShape({ ...currentShape, strokeWidth });
+    setCurrentShape((prev) => ({ ...prev, strokeWidth }));
   };
 
-  const handleMouseDown = (e: any) => {
+  const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
     setIsDrawing(true);
 
     const stage = e.target.getStage();
-    const pointerPosition = stage.getPointerPosition();
+    const pointerPosition = stage?.getPointerPosition();
 
-    if (pointerPosition) {
-      const { x, y } = pointerPosition;
+    if (!pointerPosition) return;
+    const { x, y } = pointerPosition;
 
-      switch (currentShape.drawMode) {
-        case 'freeDraw':
+    switch (currentShape.drawMode) {
+      case 'freeDraw': // 자유그리기
+        setCurrentShape((prev) => ({
+          ...prev,
+          points: [x, y],
+        }));
+        break;
+
+      case 'straightLine': //직선
+        setCurrentShape((prev) => ({
+          ...prev,
+          points: [x, y, x, y], // 시작점과 임시 끝점 설정
+        }));
+        break;
+
+      case 'ellipse': // 타원
+      case 'rectangle': // 직사각형
+        setCurrentShape((prev) => ({
+          ...prev,
+          points: [x, y],
+        }));
+        break;
+
+      case 'polygon': //다각형
+        // 첫 번째 점 근처를 클릭하면 다각형 완성
+        if (
+          currentShape.points.length >= 4 &&
+          Math.sqrt(
+            Math.pow(currentShape.points[0] - x, 2) +
+              Math.pow(currentShape.points[1] - y, 2)
+          ) < 10
+        ) {
+          setShapes([...shapes, currentShape]); // 다각형 저장
+          historyStep.current = [];
+
+          setCurrentShape((prev) => ({
+            ...prev,
+            points: [], // 초기화
+          }));
+          setIsDrawing(false);
+          return;
+        }
+
+        // 첫 클릭에서 첫 점 추가
+        if (currentShape.points.length === 0) {
           setCurrentShape((prev) => ({
             ...prev,
             points: [x, y],
           }));
-          break;
-
-        case 'straightLine':
-          setCurrentShape((prev) => ({
-            ...prev,
-            points: [x, y, x, y], // 시작점과 임시 끝점 설정
-          }));
-          break;
-
-        case 'ellipse':
-        case 'rectangle':
-          setCurrentShape((prev) => ({
-            ...prev,
-            points: [x, y],
-          }));
-          break;
-
-        case 'polygon':
-          // 첫 번째 점 근처를 클릭하면 다각형 완성
-          if (
-            currentShape.points.length >= 4 &&
-            Math.sqrt(
-              Math.pow(currentShape.points[0] - x, 2) +
-                Math.pow(currentShape.points[1] - y, 2)
-            ) < 10
-          ) {
-            setShapes([...shapes, currentShape]); // 다각형 저장
-            historyStep.current = [];
-
-            setCurrentShape((prev) => ({
-              ...prev,
-              points: [], // 초기화
-            }));
-            setIsDrawing(false);
-            return;
-          }
-
-          // 첫 클릭에서 첫 점 추가
-          if (currentShape.points.length === 0) {
-            setCurrentShape((prev) => ({
-              ...prev,
-              points: [x, y],
-            }));
-          } else {
-            // 이후 점 추가
-            setCurrentShape((prev) => ({
-              ...prev,
-              points: [...prev.points, x, y],
-            }));
-          }
-          break;
-      }
-    }
-  };
-
-  const handleMouseMove = (e: any) => {
-    if (!isDrawing || currentShape.points.length === 0) return;
-
-    const stage = e.target.getStage();
-    const pointerPosition = stage.getPointerPosition();
-
-    if (pointerPosition) {
-      const { x, y } = pointerPosition;
-
-      switch (currentShape.drawMode) {
-        case 'freeDraw':
+        } else {
+          // 이후 점 추가
           setCurrentShape((prev) => ({
             ...prev,
             points: [...prev.points, x, y],
           }));
           break;
-
-        case 'straightLine':
-          setCurrentShape((prev) => ({
-            ...prev,
-            points: [prev.points[0], prev.points[1], x, y], // 시작점과 현재 마우스 위치로 업데이트
-          }));
-          break;
-
-        case 'ellipse':
-          const radiusX = Math.abs(x - currentShape.points[0]);
-          const radiusY = Math.abs(y - currentShape.points[1]);
-          setCurrentShape((prev) => ({
-            ...prev,
-            radiusX,
-            radiusY,
-          }));
-          break;
-
-        case 'rectangle':
-          const width = x - currentShape.points[0];
-          const height = y - currentShape.points[1];
-          setCurrentShape((prev) => ({
-            ...prev,
-            width: Math.abs(width),
-            height: Math.abs(height),
-            points: [prev.points[0], prev.points[1]],
-          }));
-          break;
-
-        case 'polygon':
-          if (isDrawing && currentShape.points.length >= 2) {
-            const updatedPoints = [
-              ...currentShape.points.slice(0, -2), // 기존 점 유지
-              x,
-              y, // 현재 마우스 위치
-            ];
-            setCurrentShape((prev) => ({
-              ...prev,
-              points: updatedPoints,
-            }));
-          }
-          break;
-      }
+        }
     }
   };
 
+  const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
+    if (!isDrawing || currentShape.points.length === 0) return;
+
+    const stage = e.target.getStage();
+    const pointerPosition = stage?.getPointerPosition();
+
+    if (!pointerPosition) return;
+    const { x, y } = pointerPosition;
+
+    switch (currentShape.drawMode) {
+      // 자유그리기
+      case 'freeDraw':
+        setCurrentShape((prev) => ({
+          ...prev,
+          points: [...prev.points, x, y],
+        }));
+        break;
+
+      case 'straightLine': // 직선
+        setCurrentShape((prev) => ({
+          ...prev,
+          points: [prev.points[0], prev.points[1], x, y], // 시작점과 현재 마우스 위치로 업데이트
+        }));
+        break;
+
+      case 'ellipse': // 타원
+        const radiusX = Math.abs(x - currentShape.points[0]);
+        const radiusY = Math.abs(y - currentShape.points[1]);
+        setCurrentShape((prev) => ({
+          ...prev,
+          radiusX,
+          radiusY,
+        }));
+        break;
+
+      case 'rectangle': // 직사각형
+        const width = x - currentShape.points[0];
+        const height = y - currentShape.points[1];
+        setCurrentShape((prev) => ({
+          ...prev,
+          width: Math.abs(width),
+          height: Math.abs(height),
+          points: [prev.points[0], prev.points[1]],
+        }));
+        break;
+
+      case 'polygon': //다각형
+        if (isDrawing && currentShape.points.length >= 0) {
+          const updatedPoints = [
+            ...currentShape.points.slice(0, -2), // 기존 점 유지
+            x,
+            y, // 현재 마우스 위치
+          ];
+          setCurrentShape((prev) => ({
+            ...prev,
+            points: updatedPoints,
+          }));
+        }
+        break;
+    }
+  };
+
+  // 마우스 업 이벤트 처리하여 도형 완성
   const handleMouseUp = () => {
     if (
       currentShape.drawMode !== 'polygon' &&
@@ -199,9 +221,10 @@ const App = () => {
         currentShape.width ||
         currentShape.radiusX)
     ) {
-      setShapes([...shapes, currentShape]);
+      setShapes([...shapes, currentShape]); // 도형이 완성되면 도형 리스트에 추가
       historyStep.current = [];
       setCurrentShape((prev) => ({
+        // 현재 그려지고 있는 도형 초기화
         ...prev,
         points: [],
         radiusX: 0,
@@ -213,16 +236,16 @@ const App = () => {
     }
   };
 
+  // 모든 도형을 제거하고 현재 도형 초기화
   const handleRemoveShapes = () => {
     setShapes([]);
     historyStep.current = [];
-    setCurrentShape({ ...currentShape, points: [0, 0] });
+    setCurrentShape((prev) => ({ ...prev, points: [0, 0] }));
   };
 
-  // undo
+  // (Undo) 마지막으로 그려진 도형 실행 취소
   const handleClickUndo = () => {
-    if (shapes.length === 0) return;
-    if (historyStep.current.length > 39) return;
+    if (shapes.length === 0 || historyStep.current.length > 39) return;
 
     // shape 에서 pop
     const lastShape = shapes[shapes.length - 1];
@@ -232,16 +255,13 @@ const App = () => {
     historyStep.current.push(lastShape);
   };
 
-  // redo
+  // (Redo) 실행 취소된 도형 다시 그리기
   const handleClickRedo = () => {
-    if (historyStep.current.length === 0) return;
-
     const lastShape = historyStep.current.pop();
-    if (!lastShape) return;
-    setShapes([...shapes, lastShape]);
+    if (lastShape) setShapes((prev) => [...prev, lastShape]);
   };
 
-  // 도형 추가될 때 마다 로컬스트로지에 저장
+  // `shapes` 상태가 변경될 때마다 로컬 스토리지에 저장
   useEffect(() => {
     localStorage.setItem('shapes', JSON.stringify(shapes));
   }, [shapes]);
@@ -252,7 +272,7 @@ const App = () => {
         {/* 헤더 */}
         <Header />
 
-        {/* Nav */}
+        {/* Toolbar */}
         <DrawingToolBar
           shapes={shapes}
           currentShape={currentShape}
